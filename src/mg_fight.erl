02975c285967f3dcefd46f01fdb18efa95ace02c
yuanxiaopeng <fishballian@qq.com>
2016-05-12 11:46:54 +0800

    xxx

--- src/mg_fight.erl
+++ src/mg_fight.erl
@@ -13,6 +13,32 @@
 -include_lib("eunit/include/eunit.hrl").
 -endif.
 
+-record(fight_log, {id :: non_neg_integer(), 
+                    left,
+                    right,
+                    result :: non_neg_integer(),
+                    mods = [] :: [mod()],
+                    rounds = [] :: list()}).
+
+-record(round_log, {num :: non_neg_integer(),
+                    events :: list()}).
+
+-record(attack_event, {src_pos :: non_neg_integer(),
+                     target_effects:: list(),
+                     skill_id :: non_neg_integer()}).
+
+-record(newpet_event, {new_pets :: list()}).
+        
+
+-record(effect, {pos :: non_neg_integer(),
+                 type :: non_neg_integer(), 
+                 value :: any()}).
+
+
+
+-record(fight_data, {left, right, round}).
+
+
 %% API
 -export([pk/3]).
 -export([pk2/5]). %% @todo del
@@ -27,13 +53,15 @@ pk(_LeftInfo, _RightInfo, _Mods) ->
 
 %% Internel
 %% right all dead, left win
-pk2(_LDonePets, _LReadyPets, [], [], _Mods) ->
+pk2(_LDonePets, _LReadyPets, [], [], _Fight) ->
     left_win;
 %% left all dead, right win
-pk2([], [], _RDonePets, _ReadyPets, _Mods) ->
+pk2([], [], _RDonePets, _ReadyPets, _Fight) ->
     right_win;
-pk2(_LDonePets, _LReadyPets, _RDonePets, _ReadyPets, _Mods) ->
-    next_round.
+pk2(_LReadyPets, [], _RDonePets, [], _Fight) ->
+    next_round;
+pk2(_LDonePets, _LReadyPets, _RDonePets, _ReadyPets, _Fight) ->
+    next_attack.
 
 %% Eunit
 -ifdef(TEST).
